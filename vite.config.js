77b6acd236e07d599e7a184fce7633910b42e83a
Radit7171 <raditjal717@gmail.com>
2025-12-30 16:47:56 +0700

    fitur spa

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
     server: {
+        host: '0.0.0.0',
+        port: 5173, // DEFAULT VITE PORT (jangan 8000)
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ['**/storage/**'],
         },
     },
 });
